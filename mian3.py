2c19eac0d47b7a279be889969b737653cb4a5469
DaiHuaiYu <102659522+HuaiYDai@users.noreply.github.com>
2022-04-14 14:58:42 +0800

    cloudmusic
    文件命名不能用的字符没指定

---
+++ mian3.py
@@ -0,0 +1,39 @@
+import time
+
+import requests
+import re
+import os
+
+filename = 'music\\'
+if not os.path.exists(filename):
+    os.mkdir(filename)
+
+headers = {
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36 Edg/100.0.1185.36'
+}
+
+toplisturl = "https://music.163.com/discover/toplist"
+rs = requests.get(toplisturl,headers=headers).text
+
+toplist_url = re.findall('<a href="/discover/toplist\?id=(\d+)"(.*?)</a>',rs)
+
+
+for toplist_id,toplist_name in toplist_url:
+    toplist_name = toplist_name.replace(' class="s-fc0">','')
+    print('当前榜单    '+toplist_name)
+    url = f"https://music.163.com/discover/toplist?id={toplist_id}"
+    response = requests.get(url,headers=headers)
+    # print(response.text)
+
+    html_data = re.findall('<li><a href="/song\?id=(\d+)">(.*?)</a></li>',response.text)
+
+    for music_id,title in html_data:
+        music_url = f"https://music.163.com/song/media/outer/url?id={music_id}.mp3" #网易云接口
+        music_content = requests.get(url = music_url,headers=headers).content
+        if '/' in title:
+            title = title.replace('/','&')
+        with open("music\\"+title+'.mp3','wb') as f:
+            f.write(music_content)
+        print(title+"     下载成功")
+    print("爬取一个榜单后休息10秒")
+    time.sleep(10)
